9cf8f94086e255c3e9914c6ac125717b623ed798
Anhelina Bohdanova <gelyabogdanova88@gmail.com>
2023-09-19 18:17:48 +0300

    Start working on auth helpers

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,17 @@ const compression_1 = __importDefault(require("compression"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({ credentials: true }));
 app.use((0, compression_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
-const server = http_1.default.createServer(app);
-server.listen(3000, () => console.log('Server is running on http://localhost:3000/'));
+// const server = http.createServer(app)
+app.listen(3000, () => console.log('Server is running on http://localhost:3000/'));
+app.get('/', (req, res) => res.send('Hello!'));
+const MONGO_URL = 'mongodb+srv://anhelina:anhelina@atlascluster.i1l107b.mongodb.net/?retryWrites=true&w=majority';
+mongoose_1.default.Promise = Promise;
+mongoose_1.default.connect(MONGO_URL);
+mongoose_1.default.connection.on('error', (error) => console.log('MongoDB connection error', error));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
 import http from 'http'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -16,4 +17,13 @@ const server = http.createServer(app)
 
 server.listen(3000, () =>
   console.log('Server is running on http://localhost:3000/'),
+)
+
+const MONGO_URL =
+  'mongodb+srv://anhelina:anhelina@atlascluster.i1l107b.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error', (error: Error) =>
+  console.log('MongoDB connection error', error),
 )
