e3fa4b526c62bfe56efc9aa1c2674cf071efd37d
Anhelina Bohdanova <gelyabogdanova88@gmail.com>
2023-09-19 10:59:17 +0300

    Added server

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const compression_1 = __importDefault(require("compression"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ credentials: true }));
+app.use((0, compression_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use(body_parser_1.default.json());
+const server = http_1.default.createServer(app);
+server.listen(3000, () => console.log('Server is running on http://localhost:3000/'));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-console.log('hey')
-console.log('hey')
-console.log('hey')
-console.log('hey')
-console.log('TypeScript Eslint Prettier Starter Template!')
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express from 'express'
+import http from 'http'
+
+const app = express()
+
+app.use(cors({ credentials: true }))
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(3000, () =>
+  console.log('Server is running on http://localhost:3000/'),
+)
